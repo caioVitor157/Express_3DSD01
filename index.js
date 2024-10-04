23e0e60ade3644b3636b1734a09dd2c2a0f95f92
caio vitor <130380738+caioVitor157@users.noreply.github.com>
2024-10-04 11:38:57 +0000

    criou rota 404 e iniciou o menu

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ const express = require("express");
 const app = express();
 
 const PORT = 3333; //65K
+//CRIAÇÃO DAS NOTAS
+app.use(express.static(__dirname +"public")) {
+  res.sendFile(__dirname + "/src/views/index.html");
+};
 
 app.get("/", function (req, res) {
-  res.send("KKKKK");
+  res.sendFile(__dirname + "/src/views/index.html");
 });
-
+//CRIAÇÃO ROTA 404 (MIDOLEWARE)
+app.use(function (req, res) {
+  res.status(404).sendFile(__dirname + "/src/views/404.html");
+});
 app.listen(PORT, () => {
   console.log("running..." + PORT);
 });
